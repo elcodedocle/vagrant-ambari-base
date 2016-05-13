fe16130be8eb07db2af9cd575a3b2d1b62867e8c
Gael Abadin <gael.abadin@gmail.com>
2016-05-13 18:03:44 +0200

    Update prepare_host.sh

--- provision/prepare_host.sh
+++ provision/prepare_host.sh
@@ -40,7 +40,7 @@ for i in $(eval echo {1..$NUMBER_OF_CLUSTER_NODES}); do
 done
 
 cp /vagrant/id_rsa.pub /home/$1/.ssh/
-cat /home/$1/.ssh/id_rsa.pub >> /home/$1/.ssh/authorized_keys
+cat /home/$1/.ssh/id_rsa.pub >> /home/$4/.ssh/authorized_keys
 
 echo "umask 022" >> /etc/profile
 echo "echo 'never' > /sys/kernel/mm/transparent_hugepage/defrag" >> /etc/rc.local
